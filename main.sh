4019fa46848ddf458bfa077e66c2820823e75797
Jorge Arreola <jorgitoarreolas@gmail.com>
2021-03-30 17:32:56 +0000

    .in and .out files support to check tests results

--- main.sh
+++ main.sh
@@ -9,38 +9,64 @@ cyn=$'\e[1;36m'
 end=$'\e[0m'
 clear
 for program in ./programas/*; do
-  programName=$(basename $program);
-  extension=${program##*.};
+  programName=$(basename $program)
+  extension=${program##*.}
 
   #ignore files if name begins with "_"
   if [[ ${programName:0:1} != "_" ]]; then
     # cpp programs
     if [[ $extension == "cpp" ]]; then
-      printf "${cyn}$programName${end} ${blu}(c++)${end}\n";
+      printf "${cyn}$programName${end} ${blu}(c++)${end}\n"
       clang++-7 -pthread -std=c++17 -o main $program
       if [ -e "main" ]; then
-        for testFile in ./casos/*.txt; do
-          testFilename=$(basename $testFile .txt)
+        for testFile in ./casos/*.in; do
+          testFilename=$(basename $testFile .in)
           if [[ ${testFilename:0:1} != "_" ]] ; then
-            printf "${yel}► $testFilename${end}\n";  
-            ./main < $testFile
-            printf "\n" ;
+            printf "${yel}► $testFilename${end}\n"
+            out=$(./main < $testFile)
+            echo "$out"
+
+            # compare with expected output (.out files)
+            if [ -e "./casos/$testFilename.out" ]; then
+              outputFilename="./casos/$testFilename.out"
+              expected=$(cat $outputFilename)
+              if [[ $out == $expected ]]; then
+                printf "${grn}AC${end}"
+              else
+                printf "${red}WA${end}"
+              fi
+            fi
+            printf "\n" 
+
           fi
         done
-        rm ./main;
+        rm ./main
       fi
-      echo;
+      echo
     fi
 
     # python programs
     if [[ $extension == "py" ]]; then
-      printf "${cyn}$programName${end} ${yel}(python)${end}\n";
-      for testFile in ./casos/*.txt; do
-        testFilename=$(basename $testFile .txt)
+      printf "${cyn}$programName${end} ${yel}(python)${end}\n"
+      for testFile in ./casos/*.in; do
+        testFilename=$(basename $testFile .in)
         if [[ ${testFilename:0:1} != "_" ]] ; then
           printf "${yel}► $testFilename${end}\n";  
-          python $program < $testFile
-          printf "\n" ;
+          out=$(python $program < $testFile)
+          echo "$out"
+
+          # compare with expected output (.out files)
+            if [ -e "./casos/$testFilename.out" ]; then
+              outputFilename="./casos/$testFilename.out"
+              expected=$(cat $outputFilename)
+              if [[ $out == $expected ]]; then
+                printf "${grn}AC${end}"
+              else
+                printf "${red}WA${end}"
+              fi
+            fi
+            printf "\n\n" 
+
         fi
       done
     fi
